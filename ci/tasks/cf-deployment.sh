2736f6c633362ceed9548bc6fcaa31af67821523
Rahul Jain <rjain@pivotal.io>
2016-10-12 08:31:58 -0500

    updated env variables

--- ci/tasks/cf-deployment.sh
+++ ci/tasks/cf-deployment.sh
@@ -18,21 +18,21 @@ push="cf push metrics -n $HOST -p metrics-release/metrics-*.jar -m 512m --no-sta
 #echo $push
 eval $push
 
-cf set-env SPRING_PROFILES_ACTIVE jmx
-cf set-env JBP_CONFIG_CONTAINER_CERTIFICATE_TRUST_STORE '{enabled: true}'
-cf set-env jmx.host 172.16.1.46
-cf set-env jmx.port 44444
-cf set-env jmx.username jmxadmin
-cf set-env jmx.password: jmxadmin
-cf set-env cf.target $API_ENDPOINT
-cf set-env cf.username $USERNAME
-cf set-env cf.password $PASSWORD
-cf set-env mail.protocol smtp
-cf set-env mail.host smtp.sendgrid.com
-cf set-env mail.port 587
-cf set-env mail.username blackhawk
-cf set-env mail.password Welcome12#
-cf set-env mail.starttls true
-cf set-env mail.auth true
+cf set-env metrics SPRING_PROFILES_ACTIVE jmx
+cf set-env metrics JBP_CONFIG_CONTAINER_CERTIFICATE_TRUST_STORE '{enabled: true}'
+cf set-env metrics jmx.host 172.16.1.46
+cf set-env metrics jmx.port 44444
+cf set-env metrics jmx.username jmxadmin
+cf set-env metrics jmx.password: jmxadmin
+cf set-env metrics cf.target $API_ENDPOINT
+cf set-env metrics cf.username $USERNAME
+cf set-env metrics cf.password $PASSWORD
+cf set-env metrics mail.protocol smtp
+cf set-env metrics mail.host $SMTP_HOST
+cf set-env metrics mail.port 587
+cf set-env metrics mail.username blackhawk
+cf set-env metrics mail.password Welcome12#
+cf set-env metrics mail.starttls true
+cf set-env metrics mail.auth true
 
 cf start metrics
