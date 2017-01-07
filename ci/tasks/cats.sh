08eebc3227b07cbc65628f57c74140c4bd279eab
Rahul Jain <rjain@pivotal.io>
2017-01-07 00:23:38 -0600

    Updated the scripts and the pipeline

--- ci/tasks/cats.sh
+++ ci/tasks/cats.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-get_code="curl -I $APP_URL 2>/dev/null | head -n 1 | cut -d$' ' -f2"
-status_code=`eval $get_code`
-if [ "$status_code" != "200" ]
+RESPONSE_CODE="curl -I $APP_URL 2>/dev/null | head -n 1 | cut -d$' ' -f2"
+if [ "$RESPONSE_CODE" != "200" ]
 then
-  echo "Expect status code from $APP_URL as 200, but got $status_code"
+  echo "Expect status code from $APP_URL as 200, but got $RESPONSE_CODE"
   exit 1
 fi
