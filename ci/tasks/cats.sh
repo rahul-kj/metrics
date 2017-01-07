a799ba9763ef7c0bec7d11674ab7e7d4733cd67d
Rahul Jain <rjain@pivotal.io>
2017-01-07 00:36:38 -0600

    fixed the script

--- ci/tasks/cats.sh
+++ ci/tasks/cats.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-RESPONSE_CODE="curl -I $APP_URL 2>/dev/null | head -n 1 | cut -d$' ' -f2"
+RESPONSE_CODE=`curl -I $APP_URL 2>/dev/null | head -n 1 | cut -d$' ' -f2`
 if [ "$RESPONSE_CODE" != "200" ]
 then
   echo "Expect status code from $APP_URL as 200, but got $RESPONSE_CODE"
